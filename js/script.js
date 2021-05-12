91d1e0c41b02436f4be032274b69db765916a2ac
Emmanuel Uchenna <eunithelp@gmail.com>
2021-05-12 06:50:31 +0100

    update

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@ $(function () {
 
   // Preloader
   // fadeOut the preloader in 2s after document loaded
-  let preloaderCont = $("#preloader");
+  /* let preloaderCont = $("#preloader");
   setTimeout(() => {
     preloaderCont.fadeOut(200);
   }, 2000);
-
+ */
   // Navbar toggler
   let btnToggler = $(".navbar-toggler");
   btnToggler.on("click", function () {
