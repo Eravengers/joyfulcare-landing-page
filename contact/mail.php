a585a5b21fcd0d7be70311c92610a2a73e0caee7
Emmanuel Uchenna <emmanuelu@era-tek.io>
2021-06-26 01:40:55 +0100

    feat: add mail form using php

--- contact/mail.php
+++ contact/mail.php
@@ -6,5 +6,5 @@ $recipient = "eunithelp@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+echo "Thank You!" . " -" . "<a href='index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
 ?>
