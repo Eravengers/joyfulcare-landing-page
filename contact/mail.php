434463665e3c2cf012ab6039870403341b889406
Emmanuel Uchenna <emmanuelu@era-tek.io>
2021-06-26 02:06:35 +0100

    fixup! feat: add mail form using php

--- contact/mail.php
+++ contact/mail.php
@@ -1,10 +1,334 @@
-<?php $name = $_POST['name'];
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>Joyful Care | Homemaking & Companionship</title>
+		<meta name="description" content="Joyful Care | Homemaking & Companionship">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="title" content="Joyful Care | Homemaking & Companionship">
+		<meta name="author" content="@bacfinc">
+		<meta name="description" content="Joyful Care | Homemaking & Companionship">
+		<meta name="keywords" content="">
+		<meta http-equiv="Content-Type" content="text/html">
+		<meta name="theme-color" content="#285185">
+		<link rel="manifest" href="manifest.json">
+		<meta name="HandheldFriendly" content="True">
+		<meta name="MobileOptimized" content="320">
+		<meta name="mobile-web-app-capable" content="yes">
+		<meta name="apple-touch-fullscreen" content="yes">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-status-bar-style" content="#285185">
+		<link rel="apple-touch-icon" href="img/favicons/logo.png">
+		<meta name="msapplication-TileColor" content="#285185">
+		<meta name="msapplication-TileImage" content="img/favicons/logo.png">
+		<meta name="twitter:card" content="summary_large_image">
+		<meta name="twitter:site" content="@">
+		<meta name="twitter:creator" content="@bacfinc">
+		<meta name="twitter:title" content="Joyful Care | Homemaking & Companionship">
+		<meta name="twitter:description" content="Joyful Care | Homemaking & Companionship">
+		<meta name="twitter:image" content="img/social-banner-big.png">
+		<meta name="apple-mobile-web-app-title" content="Add to Home">
+
+		<!-- Open Graph data -->
+		<meta property="og:title" content="Joyful Care | Homemaking & Companionship">
+		<meta property="og:url" content="https://joyfulcarect.com">
+		<meta property="og:description" content="Joyful Care | Homemaking & Companionship">
+		<meta property="og:site_name" content="Joyful Care | Homemaking & Companionship">
+		<meta property="og:image" content="img/favicons/logo.png">
+		<meta name="og:locale" content="en_UK">
+		<meta name="fb:admins" content="XXXXXXXXXXXXXXXXX">
+		<meta name="og:type" content="website">
+		<meta property="article:publisher" content="https://www.facebook.com/joyfulcare">
+		<meta property="og:image:secure_url" content="img/social-banner-big.png">
+		<meta property="og:image:width" content="512">
+		<meta property="og:image:height" content="512">
+
+		<link href="https://fonts.gstatic.com" crossorigin rel="preconnect" />
+
+		<!-- Site Verifications -->
+		<meta name="google-site-verification" content="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" />
+		<meta name="google-analytics" content="UA-XXXXXXX-X">
+
+		<!--  Stylesheets -->
+
+		<!-- Custom stylesheets -->
+		<link rel="stylesheet" href="css/styles.css">
+		<link rel="stylesheet" href="css/typography.css">
+
+		<!-- Vendor stylesheets -->
+		<link rel="stylesheet" type="text/css" media="all" href="css/vendor/reset.min.css">
+		<link rel="stylesheet" type="text/css" media="all" href="css/vendor/animate.min.css">
+		<link rel="stylesheet" type="text/css" media="all" href="css/vendor/linearicons.min.css">
+		<link rel="stylesheet" type="text/css" media="all" href="css/vendor/font-awesome.min.css">
+		<link rel="stylesheet" href="css/vendor/bootstrap.min.css" />
+		<link rel="stylesheet" href="css/vendor/owl.carousel.min.css" />
+		<link rel="stylesheet" href="css/vendor/jquery-ui.css" />
+		<link rel="stylesheet" href="css/vendor/buorg.css" />
+
+
+		<!-- favicon -->
+		<link rel="shortcut icon" type="image/x-icon" href="img/favicons/favicon.ico">
+		<link rel="shortcut icon" sizes="16x16" href="img/favicons/favicon-16.ico">
+		<link rel="shortcut icon" sizes="32x32" href="img/favicons/favicon-32.ico">
+		<link rel="shortcut icon" sizes="64x64" href="img/favicons/favicon-64.ico">
+		<link rel="shortcut icon" sizes="72x72" href="img/favicons/favicon-72.ico">
+		<link rel="shortcut icon" sizes="92x92" href="img/favicons/favicon-92.ico">
+		<link rel="shortcut icon" sizes="96x96" href="img/favicons/favicon-96.ico">
+		<link rel="shortcut icon" sizes="120x120" href="img/favicons/favicon-120.ico">
+		<link rel="shortcut icon" sizes="144x144" href="img/favicons/favicon-144.ico">
+		<link rel="shortcut icon" sizes="192x192" href="img/favicons/favicon-192.ico">
+		<link rel="shortcut icon" sizes="512x512" href="img/favicons/favicon-512.ico">
+		<link rel="apple-touch-icon" sizes="16x16" href="img/favicons/favicon-16.ico">
+		<link rel="apple-touch-icon" sizes="32x32" href="img/favicons/favicon-32.ico">
+		<link rel="apple-touch-icon" sizes="64x64" href="img/favicons/favicon-64.ico">
+		<link rel="apple-touch-icon" sizes="72x72" href="img/favicons/favicon-72.ico">
+		<link rel="apple-touch-icon" sizes="92x92" href="img/favicons/favicon-92.ico">
+		<link rel="apple-touch-icon" sizes="96x96" href="img/favicons/favicon-96.ico">
+		<link rel="apple-touch-icon" sizes="120x120" href="img/favicons/favicon-120.ico">
+		<link rel="apple-touch-icon" sizes="144x144" href="img/favicons/favicon-144.ico">
+		<link rel="apple-touch-icon" sizes="192x192" href="img/favicons/favicon-192.ico">
+		<link rel="apple-touch-icon" sizes="512x512" href="img/favicons/favicon-512.ico">
+		<link rel="apple-touch-icon-precomposed" href="img/favicons/logo.png">
+		<link rel="apple-touch-icon" href="img/favicons/logo.png">
+		<link rel="mask-icon" href="img/favicons/logo.png" color="#285185">
+
+
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+		<!-- Google Tag Manager -->
+		<script>
+			(function (w, d, s, l, i) {
+				w[l] = w[l] || [];
+				w[l].push({
+					'gtm.start': new Date().getTime(),
+					event: 'gtm.js'
+				});
+				var f = d.getElementsByTagName(s)[0],
+					j = d.createElement(s),
+					dl = l != 'dataLayer' ? '&l=' + l : '';
+				j.async = true;
+				j.src =
+					'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+				f.parentNode.insertBefore(j, f);
+			})(window, document, 'script', 'dataLayer', 'GTM-XXXXXX');
+		</script>
+		<!-- End Google Tag Manager -->
+		<script>
+			(function (i, s, o, g, r, a, m) {
+				i['GoogleAnalyticsObject'] = r;
+				i[r] = i[r] || function () {
+					(i[r].q = i[r].q || [])
+					.push(arguments)
+				}, i[r].l = 1 * new Date();
+				a = s.createElement(o),
+					m = s.getElementsByTagName(o)[0];
+				a.async = 1;
+				a.src = g;
+				m.parentNode.insertBefore(a, m)
+			})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+			ga('create', 'UA-XXXXXXXX-X', 'auto');
+			ga('send', 'pageview');
+		</script>
+
+		<!-- Google Tag Manager (noscript) -->
+		<noscript>
+			<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXX" height="0" width="0"
+				style="display:none;visibility:hidden"></iframe>
+		</noscript>
+		<!-- End Google Tag Manager (noscript) -->
+	</head>
+
+	<body>
+		<!-- Preloader -->
+		<!-- Remove displayNone to display preloader -->
+		<div id="preloader" class="displayNone">
+			<div class="spinner"></div>
+		</div>
+		<!-- /Preloader -->
+
+		<!-- Content start -->
+
+		<!-- Header -->
+		<header id="top">
+
+			<div class="container">
+				<!-- Navigation -->
+				<nav class="navbar navbar-expand-lg">
+					<a class="navbar-brand justify-start" href="index.html">
+						<img src="assets/Logo.svg" class="logo" alt="Joyful care logo" />
+					</a>
+					<button class="navbar-toggler noOutline" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
+						aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="lnr lnr-cross displayNone"></span>
+						<span class="lnr lnr-menu"></span>
+					</button>
+
+					<div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+						<ul class="navbar-nav">
+							<li class="nav-item">
+								<a class="nav-link mr-4 js-scroll active" href="#home">Home <span class="sr-only">(current)</span></a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link mr-4 js-scroll" href="#services">Our Services</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link mr-4 js-scroll" href="#about">About Us</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link mr-4 js-scroll button contact-us-btn" href="#contact">Contact Us</a>
+							</li>
+						</ul>
+					</div>
+				</nav>
+				<!-- /Navigation -->
+			</div>
+		</header>
+		<!-- /Header -->
+		<?php $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
 $recipient = "eunithelp@gmail.com";
+// $recipient = "info@joyfulcarect.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!" . " -" . "<a href='index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+echo "Thank You! Your message was sent sucessfully" . " -" . "<a href='index.html' class='cta-btn cta-link'> Return Home</a>";
 ?>
+
+		<body>
+
+
+			<!-- Footer -->
+			<footer class="footer" id="footer">
+				<div class="container">
+					<div class="row">
+
+						<!-- First column -->
+						<div class="col-sm-12 col-md-3 col-lg-4">
+							<div class="footer-logo">
+								<img src="assets/Logo footer.svg" alt="Joyful Care logo" class="">
+							</div>
+							<address class="footer-address">
+								14 Glen Pl, Bridgeport CT 06610
+							</address>
+							<div class="legal">
+								<span class="copyright" id="copyright">
+								</span>
+								<span class="legal-link">
+									<a href="privacy.html">Privacy</a>
+									-
+									<a href="terms.html">Terms</a>
+								</span>
+							</div>
+
+							<!-- Social media icons -->
+							<div class="stay-connected d-inline">
+								<div class="social-icons">
+									<a href="#" title="Joyful Care on Facebook" class="social-icon">
+										<img class="social-icon" src="assets/Icons/social-icons/facebook-icon.svg"
+											alt="Joyful Care on Facebook" srcset="assets/Icons/social-icons/facebook-icon.svg">
+									</a>
+
+									<a href="#" title="Joyful Care on Twitter" class="social-icon">
+										<img class="social-icon" src="assets/Icons/social-icons/twitter-icon.svg"
+											alt="Joyful Care on Twitter" srcset="assets/Icons/social-icons/twitter-icon.svg">
+									</a>
+
+									<a href="#" title="Joyful Care on Instagram" class="social-icon">
+										<img class="social-icon" src="assets/Icons/social-icons/instagram-icon.svg"
+											alt="Joyful Care on Instagram" srcset="assets/Icons/social-icons/instagram-icon.svg">
+									</a>
+								</div>
+							</div>
+
+						</div>
+						<!-- /First column -->
+
+						<!-- Second column -->
+						<div class="col-sm-12 col-md-3 col-lg-4 pd-1">
+							<div class="footer-title">
+								Service Links
+							</div>
+							<div class="footer-links-container">
+								<ul class="footer-links">
+									<li class="footer-link">
+										<a href="#home">Home</a>
+									</li>
+									<li class="footer-link">
+										<a href="#about">About Us</a>
+									</li>
+									<li class="footer-link">
+										<a href="#services">Our Services</a>
+									</li>
+									<li class="footer-link">
+										<a href="#contact">Contact Us</a>
+									</li>
+									<li class="footer-link">
+										<a href="faq.html">FAQ</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+						<!-- /Second column -->
+
+						<!-- Third column -->
+						<div class="col-sm-12 col-md-3 col-lg-4">
+							<div class="footer-title">
+								Newsletter
+							</div>
+							<div class="newsletter-text">
+								Sign up for free newsletters
+							</div>
+							<div class="newsletter-input-box">
+								<input type="email" name="email" placeholder="Your Email Address" aria-placeholder="Your Email Address"
+									class="newsletter-input">
+								<button class="button newsletter-button">
+									Subscribe
+								</button>
+							</div>
+
+						</div>
+
+						<!-- /Third column -->
+
+					</div>
+				</div>
+			</footer>
+			<!-- /Footer -->
+
+			<!-- ScrollToTop Button -->
+			<a class="btn scrollToTop hideScrollTop" href="#top">
+				<img src="assets/Scroll up.svg" alt="Scroll top">
+			</a>
+			<!-- ScrollToTop Button END-->
+
+			<!-- Vendor scripts -->
+			<!-- <script src="js/vendor/jquery-3.1.1.min.js"></script> -->
+			<script src="js/vendor/bootstrap.min.js"></script>
+			<script src="js/vendor/jquery-ui.js"></script>
+			<script src="js/vendor/magnific-popup.min.js"></script>
+			<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+				integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+			</script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+				integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+			</script>
+			<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"
+				integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous">
+			</script>
+			<script src="js/vendor/buorg.js"></script>
+
+
+			<!-- Custom script -->
+			<script src="js/script.js" async defer></script>
+
+
+		</body>
+
+</html>
