dce4504b56ed6ee984ecec93106febf44d1c01df
Emmanuel Uchenna <emmanuelu@era-tek.io>
2021-07-16 18:03:54 +0100

    fix: add validation for contact us section in homepage with coloring of input fields

--- js/validation.js
+++ js/validation.js
@@ -1,22 +1,22 @@
 function validateName() {
 
-	var name = document.getElementById('formName').value;
+	var name = document.getElementById("formName").value;
 
 	if (name.length === 0) {
 
-		producePrompt('Name is required', 'name-error', 'red');
+		producePrompt("Name is required", "name-error", "#d9534f");
 		return false;
 
 	}
 
 	if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
 
-		producePrompt('First and last name, please.', 'name-error', 'red');
+		producePrompt("First and last name, please.", "name-error", "#d9534f");
 		return false;
 
 	}
 
-	producePrompt('Valid', 'name-error', 'green');
+	producePrompt("Valid", "name-error", "#5cb85c");
 	return true;
 
 }
@@ -25,48 +25,73 @@ function validateName() {
 
 function validateEmail() {
 
-	var email = document.getElementById('formEmail').value;
+	var email = document.getElementById("formEmail").value;
 
 	if (email.length === 0) {
 
-		producePrompt('Email Invalid', 'email-error', 'red');
+		producePrompt("Email Invalid", "email-error", "#d9534f");
 		return false;
 
 	}
 
 	if (!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
 
-		producePrompt('Email Invalid', 'email-error', 'red');
+		producePrompt("Invalid Email", "email-error", "#d9534f");
 		return false;
 
 	}
 
-	producePrompt('Valid', 'email-error', 'green');
+	producePrompt("Valid", "email-error", "#5cb85c");
 	return true;
 
 }
 
 function validateMessage() {
-	var message = document.getElementById('formMessage').value;
+	var message = document.getElementById("formMessage").value;
 	var required = 30;
 	var left = required - message.length;
 
 	if (left > 0) {
-		producePrompt(left + ' more characters required', 'message-error', 'red');
+		producePrompt(left + " more characters required", "message-error", "#d9534f");
 		return false;
 	}
 
-	producePrompt('Valid', 'message-error', 'green');
+	producePrompt("Valid", "message-error", "#5cb85c");
 	// return true;
 
 }
 
 function validateForm() {
+	var nameLocation = document.getElementById("formName"),
+		emailLocation = document.getElementById("formEmail"),
+		messageLocation = document.getElementById("formMessage");
+
+	// Coloring the border of the input fields
+
+	if (!validateName()) {
+		nameLocation.style.borderColor = "#d9534f";
+	} else {
+		nameLocation.style.borderColor = "#5cb85c";
+	}
+
+	if (!validateEmail()) {
+		emailLocation.style.borderColor = "#d9534f";
+	} else {
+		emailLocation.style.borderColor = "#5cb85c";
+	}
+
+	if (!validateMessage()) {
+		messageLocation.style.borderColor = "#d9534f";
+	} else {
+		messageLocation.style.borderColor = "#5cb85c";
+	}
+
+
 	if (!validateName() || !validatePhone() || !validateEmail() || !validateMessage()) {
-		jsShow('submit-error');
-		producePrompt('Please fix errors to submit.', 'submit-error', 'red');
+		jsShow("submit-error");
+		producePrompt("Please fix errors to submit.", "submit-error", "#d9534f");
 		setTimeout(function () {
-			jsHide('submit-error');
+			jsHide("submit-error");
 		}, 2000);
 		return false;
 	} else {
@@ -75,11 +100,11 @@ function validateForm() {
 }
 
 function jsShow(id) {
-	document.getElementById(id).style.display = 'block';
+	document.getElementById(id).style.display = "block";
 }
 
 function jsHide(id) {
-	document.getElementById(id).style.display = 'none';
+	document.getElementById(id).style.display = "none";
 }
 
 
