fe25e7106fce272c704db0f4a44fea56adb52a34
Emmanuel Uchenna <emmanuelu@era-tek.io>
2021-07-22 12:35:30 +0100

    feat: - Submission of contact message fixed

--- js/validation.js
+++ js/validation.js
@@ -129,11 +129,32 @@ function validateForm() {
 		}, 2000);
 		return false;
 	} else {
-		showContactModal();
-		clearFormInputs();
-		hideContactPopup();
-		// Prevent page from reloading
-		return false;
+
+		const URL = "contact/mail.php";
+		const data = {
+			name: name,
+			phone: phone,
+			email: email,
+			desc: desc
+		};
+
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.open("POST", `${URL}`);
+		xmlhttp.setRequestHeader("Content-Type", "application/json");
+		xmlhttp.send(JSON.stringify(data));
+		xmlhttp.onreadystatechange = function () {
+			if (xmlhttp.readyState === 4) {
+				const response = JSON.parse(xmlhttp.responseText);
+				if (xmlhttp.status === 200) {
+					showContactModal();
+					clearFormInputs();
+					hideContactPopup();
+				} else {
+					console.log(`${response}`)
+				}
+			}
+		}
+
 	}
 
 }
