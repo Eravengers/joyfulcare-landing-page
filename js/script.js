f1f1731bb18e3512955e317453f3f7a070a3cfc6
Emmanuel Uchenna <emmanuelu@era-tek.io>
2021-07-25 19:26:17 +0100

    fix: fix accordion on FAQs

--- js/script.js
+++ js/script.js
@@ -12,6 +12,31 @@ $(function () {
 		btnToggler.children().toggleClass("displayNone");
 	});
 
+	// Accordion
+	var accordion = document.getElementsByClassName("accordion");
+
+	var i;
+	for (i = 0; i < accordion.length; i++) {
+		accordion[i].addEventListener("click", function () {
+			// Toggle between adding and removing active class to accordion
+			this.classList.toggle("active");
+
+			// Toggle between hiding and showing the dropdown menu
+
+			var dropDownMenu = this.nextElementSibling;
+
+			if (dropDownMenu.style.display === "block") {
+				dropDownMenu.style.display = "none";
+			} else {
+				dropDownMenu.style.display = "block";
+			}
+		});
+		// Clicking outside the accordion
+		outsideElement = this.parentElement;
+
+	}
+
+	
 	// ScrollToTop
 
 	$(window).scroll(function () {
@@ -142,29 +167,5 @@ $(function () {
 	readMoreLess();
 
 
-	// Accordion
-	var accordion = document.getElementsByClassName("accordion");
-
-	var i;
-	for (i = 0; i < accordion.length; i++) {
-		accordion[i].addEventListener("click", function () {
-			// Toggle between adding and removing active class to accordion
-			this.classList.toggle("active");
-
-			// Toggle between hiding and showing the dropdown menu
-
-			var dropDownMenu = this.nextElementSibling;
-			// dropDownMenu.slideToggle();
-
-			if (dropDownMenu.style.display === "block") {
-				dropDownMenu.style.display = "none";
-			} else {
-				dropDownMenu.style.display = "block";
-			}
-		});
-		// Clicking outside the accordion
-		outsideElement = this.parentElement;
-
-	}
 
 }(jQuery));
