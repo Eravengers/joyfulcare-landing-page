bcf9836b8f4e01436dc0ba2e6e9c273829fc3f1d
Emmanuel Uchenna <emmanuelu@era-tek.io>
2021-07-26 13:01:19 +0100

    fix: fix errors

--- js/script.js
+++ js/script.js
@@ -92,7 +92,7 @@ $(function () {
 		var modalText = button.data('text'); // Extract info from data-* attributes
 		// If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
 		// Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-		var modal = $(this)
+		var modal = $(this);
 		modal.find('.modal-title').html(modalTitle);
 		modal.find('.modal-subtitle').html(modalSubTitle);
 		modal.find('.modal-text').html(modalText);
