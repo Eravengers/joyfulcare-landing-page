ac210bd58eb0c23c9c9a3de348b23a13b49d4274
Emmanuel Uchenna <emmanuelu@era-tek.io>
2021-08-22 16:23:09 +0100

    Refactor: Beautify file

--- js/script.js
+++ js/script.js
@@ -1,148 +1,148 @@
-$(function() {
+$(function () {
 
-    // Preloader
-    // fadeOut the preloader in 2s after document loaded
-    // let preloaderCont = $("#preloader");
-    // preloaderCont.delay(2000).fadeOut(200); // hide preloaderCont in 2s
-
-
-    // Navbar toggler
-    let btnToggler = $(".navbar-toggler");
-    btnToggler.on("click", function() {
-        btnToggler.children().toggleClass("displayNone");
-    });
-
-    // ScrollToTop
-
-    $(window).scroll(function() {
-        var $scrollPosition = $(window).scrollTop(),
-            $scrollToTopArea = $(".scrollToTop");
-
-        if ($scrollPosition >= 300) {
-            $scrollToTopArea.removeClass("hideScrollTop").addClass("showScrollTop");
-        } else {
-            $scrollToTopArea.removeClass("showScrollTop").addClass("hideScrollTop");
-        }
-    });
+	// Preloader
+	// fadeOut the preloader in 2s after document loaded
+	// let preloaderCont = $("#preloader");
+	// preloaderCont.delay(2000).fadeOut(200); // hide preloaderCont in 2s
 
 
-    // scrollToTopView();
-    $(document).ready(function() {
-        // Add smooth scrolling to all links
-        $("a.js-scroll").on('click', function(event) {
+	// Navbar toggler
+	let btnToggler = $(".navbar-toggler");
+	btnToggler.on("click", function () {
+		btnToggler.children().toggleClass("displayNone");
+	});
 
-            // Make sure this.hash has a value before overriding default behavior
-            if (this.hash !== "") {
-                // Prevent default anchor click behavior
-                event.preventDefault();
+	// ScrollToTop
 
-                // Store hash
-                var hash = this.hash,
-                    offset = 300;
-                if ($(this).data('offset') != undefined) offset = $(this).data('offset');
+	$(window).scroll(function () {
+		var $scrollPosition = $(window).scrollTop(),
+			$scrollToTopArea = $(".scrollToTop");
 
-                // Using jQuery's animate() method to add smooth page scroll
-                // The optional number (300) specifies the number of milliseconds it takes to scroll to the specified area
-                $('body').animate({
-                    scrollTop: $(hash).offset().top - offset
-                }, 300, function() {
-
-                    // Add hash (#) to URL when done scrolling (default click behavior)
-                    window.location.hash = hash;
-
-                    // console.log(`Loc without Off: ${$(hash).offset().top} Loc with Off: ${$(hash).offset().top - offset} `);
-                });
-                return false;
-            } // End if
-        });
-    });
-
-    // Add current year to copyright
-    let date = new Date();
-    let copyrightYear = date.getFullYear();
-
-    $("#copyright").text(`© ${copyrightYear}`);
-
-    // Modal
-    $('#detailsModal').on('show.bs.modal', function(event) {
-        var button = $(event.relatedTarget); // Button that triggered the modal
-        var modalTitle = button.data('title'); // Extract info from data-* attributes
-        var modalSubTitle = button.data('subtitle'); // Extract info from data-* attributes
-        var modalText = button.data('text'); // Extract info from data-* attributes
-        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-        var modal = $(this)
-        modal.find('.modal-title').html(modalTitle);
-        modal.find('.modal-subtitle').html(modalSubTitle);
-        modal.find('.modal-text').html(modalText);
-    });
-
-    // Read more / Read less buttons
-
-    function readMoreLess() {
-
-        // Services section
-
-        //readMoreCompanionshipBtn
-        let readMoreCompanionshipBtn = $("#readMoreCompanionshipBtn"),
-            readMoreCompanionshipBtnTxt = $("#readMoreCompanionshipBtnTxt"),
-            readMoreCompanionshipText = $("#caretTextCompanionshipText"),
-            readMoreCompanionship = $("#readMoreCompanionship"),
-            readMoreCompanionshipCaret = $("#readMoreCompanionshipCaret");
-
-        readMoreCompanionship.slideToggle(); //hide readMoreCompanionship by default;
-        readMoreCompanionshipText.text("Read more"); //sets text to "Read more" by default
-
-        readMoreCompanionshipBtn.on("click", function() {
-            readMoreCompanionship.slideToggle(300); //toggles readMoreCompanionship when the btn is clicked
-            let newText = $(this).text().trim(); // Gets text and remove whitespace;
-            // Alternate between texts
-            if (newText === "Read more") {
-                newText = "Read less";
-                readMoreCompanionshipCaret.addClass("fa-minus-circle");
-                readMoreCompanionshipCaret.removeClass("fa-plus-circle");
-            } else {
-                newText = "Read more";
-                readMoreCompanionshipCaret.removeClass("fa-minus-circle");
-                readMoreCompanionshipCaret.addClass("fa-plus-circle");
-            }
-
-            readMoreCompanionshipText.text(newText); //sets new text
-            // For development use ONLY
-            // console.log(`Text is ${newText}`);
-        });
+		if ($scrollPosition >= 300) {
+			$scrollToTopArea.removeClass("hideScrollTop").addClass("showScrollTop");
+		} else {
+			$scrollToTopArea.removeClass("showScrollTop").addClass("hideScrollTop");
+		}
+	});
 
 
-        //readMorePcaBtn
-        let readMorePcaBtn = $("#readMorePcaBtn"),
-            readMorePcaBtnTxt = $("#readMorePcaBtnTxt"),
-            readMorePcaText = $("#caretTextPcaText"),
-            readMorePca = $("#readMorePca"),
-            readMorePcaCaret = $("#readMorePcaCaret");
+	// scrollToTopView();
+	$(document).ready(function () {
+		// Add smooth scrolling to all links
+		$("a.js-scroll").on('click', function (event) {
 
-        readMorePca.slideToggle(); //hide readMorePca by default;
-        readMorePcaText.text("Read more"); //sets text to "Read more" by default
+			// Make sure this.hash has a value before overriding default behavior
+			if (this.hash !== "") {
+				// Prevent default anchor click behavior
+				event.preventDefault();
 
-        readMorePcaBtn.on("click", function() {
-            readMorePca.slideToggle(300); //toggles readMorePca when the btn is clicked
-            let newText = $(this).text().trim(); // Gets text and remove whitespace;
-            // Alternate between texts
-            if (newText === "Read more") {
-                newText = "Read less";
-                readMorePcaCaret.addClass("fa-minus-circle");
-                readMorePcaCaret.removeClass("fa-plus-circle");
-            } else {
-                newText = "Read more";
-                readMorePcaCaret.removeClass("fa-minus-circle");
-                readMorePcaCaret.addClass("fa-plus-circle");
-            }
+				// Store hash
+				var hash = this.hash,
+					offset = 300;
+				if ($(this).data('offset') != undefined) offset = $(this).data('offset');
 
-            readMorePcaText.text(newText); //sets new text
-            // For development use ONLY
-            // console.log(`Text is ${newText}`);
-        });
-    }
+				// Using jQuery's animate() method to add smooth page scroll
+				// The optional number (300) specifies the number of milliseconds it takes to scroll to the specified area
+				$('body').animate({
+					scrollTop: $(hash).offset().top - offset
+				}, 300, function () {
 
-    readMoreLess();
+					// Add hash (#) to URL when done scrolling (default click behavior)
+					window.location.hash = hash;
+
+					// console.log(`Loc without Off: ${$(hash).offset().top} Loc with Off: ${$(hash).offset().top - offset} `);
+				});
+				return false;
+			} // End if
+		});
+	});
+
+	// Add current year to copyright
+	let date = new Date();
+	let copyrightYear = date.getFullYear();
+
+	$("#copyright").text(`© ${copyrightYear}`);
+
+	// Modal
+	$('#detailsModal').on('show.bs.modal', function (event) {
+		var button = $(event.relatedTarget); // Button that triggered the modal
+		var modalTitle = button.data('title'); // Extract info from data-* attributes
+		var modalSubTitle = button.data('subtitle'); // Extract info from data-* attributes
+		var modalText = button.data('text'); // Extract info from data-* attributes
+		// If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+		// Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+		var modal = $(this)
+		modal.find('.modal-title').html(modalTitle);
+		modal.find('.modal-subtitle').html(modalSubTitle);
+		modal.find('.modal-text').html(modalText);
+	});
+
+	// Read more / Read less buttons
+
+	function readMoreLess() {
+
+		// Services section
+
+		//readMoreCompanionshipBtn
+		let readMoreCompanionshipBtn = $("#readMoreCompanionshipBtn"),
+			readMoreCompanionshipBtnTxt = $("#readMoreCompanionshipBtnTxt"),
+			readMoreCompanionshipText = $("#caretTextCompanionshipText"),
+			readMoreCompanionship = $("#readMoreCompanionship"),
+			readMoreCompanionshipCaret = $("#readMoreCompanionshipCaret");
+
+		readMoreCompanionship.slideToggle(); //hide readMoreCompanionship by default;
+		readMoreCompanionshipText.text("Read more"); //sets text to "Read more" by default
+
+		readMoreCompanionshipBtn.on("click", function () {
+			readMoreCompanionship.slideToggle(300); //toggles readMoreCompanionship when the btn is clicked
+			let newText = $(this).text().trim(); // Gets text and remove whitespace;
+			// Alternate between texts
+			if (newText === "Read more") {
+				newText = "Read less";
+				readMoreCompanionshipCaret.addClass("fa-minus-circle");
+				readMoreCompanionshipCaret.removeClass("fa-plus-circle");
+			} else {
+				newText = "Read more";
+				readMoreCompanionshipCaret.removeClass("fa-minus-circle");
+				readMoreCompanionshipCaret.addClass("fa-plus-circle");
+			}
+
+			readMoreCompanionshipText.text(newText); //sets new text
+			// For development use ONLY
+			// console.log(`Text is ${newText}`);
+		});
+
+
+		//readMorePcaBtn
+		let readMorePcaBtn = $("#readMorePcaBtn"),
+			readMorePcaBtnTxt = $("#readMorePcaBtnTxt"),
+			readMorePcaText = $("#caretTextPcaText"),
+			readMorePca = $("#readMorePca"),
+			readMorePcaCaret = $("#readMorePcaCaret");
+
+		readMorePca.slideToggle(); //hide readMorePca by default;
+		readMorePcaText.text("Read more"); //sets text to "Read more" by default
+
+		readMorePcaBtn.on("click", function () {
+			readMorePca.slideToggle(300); //toggles readMorePca when the btn is clicked
+			let newText = $(this).text().trim(); // Gets text and remove whitespace;
+			// Alternate between texts
+			if (newText === "Read more") {
+				newText = "Read less";
+				readMorePcaCaret.addClass("fa-minus-circle");
+				readMorePcaCaret.removeClass("fa-plus-circle");
+			} else {
+				newText = "Read more";
+				readMorePcaCaret.removeClass("fa-minus-circle");
+				readMorePcaCaret.addClass("fa-plus-circle");
+			}
+
+			readMorePcaText.text(newText); //sets new text
+			// For development use ONLY
+			// console.log(`Text is ${newText}`);
+		});
+	}
+
+	readMoreLess();
 
 }(jQuery));
